554802ab39a22d94d3ebf6757bb355cb0538bc94
joshi dron <joshidron007@gmail.com>
2026-01-08 00:37:52 +0530

    all

--- mental-helth new_2 - Copy (2) - Copy/app.py
+++ mental-helth new_2 - Copy (2) - Copy/app.py
@@ -318,17 +318,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=5001)
-
-
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Prajna Path is live 🚀"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
